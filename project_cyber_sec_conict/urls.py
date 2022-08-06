329227e11ba7c8e2df38a2e82eaa0ad36f181206
Victor G. Marques <victormarques8801@gmail.com>
2022-08-05 21:41:43 -0300

    Adição da estrutura de urls do projeto

--- project_cyber_sec_conict/urls.py
+++ project_cyber_sec_conict/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include("apps.principal_cyber_sec.urls")),
+    path('sobre-nos/email/', include("apps.emails_cyber_sec.urls")),
+    path('artigos/', include("apps.artigos_cyber_sec.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
