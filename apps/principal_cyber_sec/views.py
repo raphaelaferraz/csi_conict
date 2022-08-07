dbac9c722c33b94db7ed3cbb4184709bf5d98878
Victor G. Marques <victormarques8801@gmail.com>
2022-08-07 14:37:11 -0300

    Redirecionamento na viem de index

--- apps/principal_cyber_sec/views.py
+++ apps/principal_cyber_sec/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
-    return render(request, "artigos/geral_artigos.html")
+    return redirect('artigos')
 
 
 def desenvolvedores(request):
